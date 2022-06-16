3b5e88df81e84b9acea7fe7496ba83177011822b
Blutree-Technets <68584181+Blutree-Technets@users.noreply.github.com>
2022-06-17 02:23:16 +0530

    Update audio.js

--- examples/audio.js
+++ examples/audio.js
@@ -1,4 +1,4 @@
 const player = require('play-sound')();
-player.play('/home/RPi/alarm.mp3', (err) => {
+player.play('/home/RPi/Head-Motion-Device/alarm1.mp3', (err) => {
     if (err) console.log(`Could not play sound: ${err}`);
 });
